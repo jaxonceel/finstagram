7d4238e00f918cdde31db200d17f516dae2c2248
jaxonceel <jcflam89@gmail.com>
2021-08-01 21:06:09 +0000

    W5D2

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -25,3 +25,7 @@ class FinstagramPost < ActiveRecord::Base
       self.comments.size
     end
   end
+
+  class FinstagramPost < ActiveRecord::Base
+    validates :photo_url, :user, presence: true
+  end
